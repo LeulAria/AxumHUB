3a57b0519978642738f9db50f462829e6c7926c0
LeulAria <leulsegedtariku@gmail.com>
2020-07-31 10:57:05 -1200

    chat rooms

--- router/api/socket/chat.js
+++ router/api/socket/chat.js
@@ -8,34 +8,59 @@ const formatChatMessage = require('./formatChatMessage')
 
 io.on('connection', (socket) => {
   console.log(`user ${socket.id} connected...`)
+  console.log('socket id: ', socket.id)
 
-  // join a user to specific group
-  socket.on('joinGroupChat', ({ username, userid, roomid }) => {
-    console.log('here the user to join------------------------------------------------------: ', username, userid, roomid)
-
-    socket.join(roomid)
-    // show user online
-
-    socket.broadcast.emit('userJoinedOnline', userid)
+  socket.on('testX', (msg) => {
+    console.log(msg)
+    io.emit('server1', 'send this back')
   })
 
-  socket.emit('connected', 'socket connection is enabled and i am sending this message')
 
-  socket.broadcast.emit('connected', 'user has joined chat room')
+  socket.on('joinRoom', ({ username, userid, roomid }) => {
+    console.log('the payload: -----------------', { username, userid, roomid })
+
+    socket.join(roomid)
+
+    io.to('fuck').emit('msg', username + 'this is from room id: ' + roomid)
+  })
+
 
   socket.on("sendChat", (chatPayload) => {
     // save to chat   user Id, projectName or ChatId, push to chats: [{  }]
     // Chat.findById()
+    console.log(chatPayload)
 
-    io.emit('getChatMsg', formatChatMessage(chatPayload))
+    console.log('socket rooms: ', socket.rooms)
+
+    io.to(chatPayload.roomid).emit('showChat', formatChatMessage(chatPayload))
   })
 
-  socket.on('userEvent', (msg) => {
-    console.log(msg)
-  })
 
-  socket.on('disconnect', () => {
-    io.emit('disconnected', 'user has leaved the chat')
-  })
+  // // join a user to specific group
+  // socket.on('joinGroupChat', ({ username, userid, roomid }) => {
+  //   console.log('here the user to join------------------------------------------------------: ', username, userid, roomid)
+
+  //   console.log(roomid)
+  //   socket.join(roomid)
+  //   // show user online
+
+  //   io.to('staticRoom').emit('connected', 'wtf wtf wtf')
+
+  //   socket.broadcast.to('staticRoom').emit('userJoinedOnline', userid)
+  // })
+
+  // socket.emit('connected', 'socket connection is enabled and i am sending this message')
+
+  // socket.broadcast.emit('connected', 'user has joined chat room')
+
+
+
+  // socket.on('userEvent', (msg) => {
+  //   console.log(msg)
+  // })
+
+  // socket.on('disconnect', () => {
+  //   io.emit('disconnected', 'user has leaved the chat')
+  // })
 
 })
